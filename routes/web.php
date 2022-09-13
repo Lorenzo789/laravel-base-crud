969c13a30898b8c9765135db843ffc422fe4e89e
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-09-13 17:22:38 +0200

    correct route

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('/', 'ComicController');
+Route::resource('/', 'ComicController')->name('index','homepage');
+Route::resource('/comics', 'ComicController')->name('show','comics');
 
